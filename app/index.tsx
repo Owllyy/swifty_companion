4306094b55473c391402c09df3ec7bc05ab080ca
Lucas Bourret <lucasbourret@gmail.com>
2025-07-02 15:31:33 +0200

    get_code

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useRouter } from 'expo-router';
+import * as WebBrowser from 'expo-web-browser';
+import React, { useEffect } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 
-export default function Index() {
+const discovery = {
+  authorizationEndpoint: 'https://api.intra.42.fr/oauth/authorize',
+};
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function GetAuthentificationCode() {
+  const router = useRouter();
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: 'u-s4t2ud-2a656995e76401b0747a6bc0b746e20e6107d71f198648399c528e04b89ccb1c',
+      scopes: ['public'],
+      redirectUri: makeRedirectUri()
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      router.replace({
+        pathname: '/auth',
+        params: { code: code },
+      });
+    }
+  }, [response]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Button
+        disabled={!request}
+        title="Lancer la connexion 42"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20 },
+});
